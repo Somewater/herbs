33ceda42b8f5e7f7f0d3675e1d822b9e3cf2efab
Pavel Naydenov <pav@atlantor.ru>
2014-05-04 17:34:12 +0000

    use sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,13 +66,15 @@ Herbs::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  ActionMailer::Base.smtp_settings = {
-      :user_name => 'somewater',
-      :password => 'J8flyud7Ld8740',
-      :domain => 'lektravy.com',
-      :address => 'smtp.sendgrid.net',
-      :port => 587,
-      :authentication => :plain,
-      :enable_starttls_auto => true
-  }
+  # ActionMailer::Base.smtp_settings = {
+  #     :user_name => 'somewater',
+  #     :password => 'J8flyud7Ld8740',
+  #     :domain => 'lektravy.com',
+  #     :address => 'smtp.sendgrid.net',
+  #     :port => 587,
+  #     :authentication => :plain,
+  #     :enable_starttls_auto => true
+  # }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { :host => "lektravy.com" }
 end
