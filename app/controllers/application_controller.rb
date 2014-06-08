12669d83878e0c508a13927063994306f04e5637
Pavel Naydenov <naydenov.p.v@gmail.com>
2014-06-08 22:12:07 +0400

    cloud

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :reload_settings
+  before_filter :set_cloud_style
 
   private
   def reload_settings
     Setting.reload
+  end
+
+  def set_cloud_style
+    @cloud_style = true
   end
 end
--- app/controllers/main_page_controller.rb
+++ app/controllers/main_page_controller.rb
@@ -1,8 +1,10 @@
 class MainPageController < ApplicationController
   def index
     @section = Section.main
+    @cloud_style = false
   end
 
   def not_found
+    @cloud_style = false
   end
 end
