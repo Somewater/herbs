49eb6901ddcf5d806e3416ed8334da0f7a0824ff
Pavel Naydenov <naydenov.p.v@gmail.com>
2014-06-15 21:18:54 +0400

    use jquery 1.8

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require ./jquery
+//= require ./jquery-1.8.3.min
 //= require ./jquery.mousewheel-3.0.6.pack
 //= require ./modernizr-2.6.2.min
 //= require basket
